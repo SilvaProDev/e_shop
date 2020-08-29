3ff6a50078485962492adcb98807078ca27ec0ea
Daouda Ouattara <60108202+Daouda46@users.noreply.github.com>
2020-08-29 16:29:33 +0000

    admin newsletter

--- newsletter/admin.py
+++ newsletter/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import NewsletterUser, Newsletter
+from newsletter.models import NewsletterUser, Newsletter
 
 class NewsletterUserAdmin(admin.ModelAdmin):
     list_display = (
